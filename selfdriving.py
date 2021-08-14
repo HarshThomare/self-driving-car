a7aaa1be7c5e4db4438e4e0c27b2237738f40c23
Harsh Thomare <hthomare@gmail.com>
2021-08-13 23:51:33 -0700

    Imporoving readibility

--- selfdriving.py
+++ selfdriving.py
@@ -121,20 +121,7 @@ def create_model():
     return model
 
 
-if __name__ == '__main__':
-    data = DataInfo()
-    data = balanceData(data)
-    imgpath, steering = loadData('', data)
-    x_train, x_test, y_train, y_test = train_test_split(imgpath, steering, test_size=0.2, random_state=5)
-    model = create_model()
-    history = model.fit(batch_gen(x_train, y_train, 100, True), steps_per_epoch=300, epochs=10, validation_data=batch_gen(x_test, y_test, 100, False), validation_steps=200, verbose=2)
-    model.save('model.h5')
 
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.legend(['Training', 'Validation'])
-    plt.title('Loss')
-    plt.xlabel('Epoch')
-    plt.show()
+    
     
 
