af6378b98aaf0e6e545fb54af88086b1c4e21a30
HarshThomare <40192002+HarshThomare@users.noreply.github.com>
2021-08-23 15:10:58 -0700

    Update selfdriving.py

--- selfdriving.py
+++ selfdriving.py
@@ -12,38 +12,40 @@ import random
 
 
 
-def DataInfo():
+def DataImport():
     col = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Brake', 'Speed']
-    data = pd.read_csv('driving_log.csv', names=col)
-    data['Center'] = data['Center'].apply(lambda name: Path(name).name)
-    data['Left'] = data['Left'].apply(lambda name: Path(name).name)
-    data['Right'] = data['Right'].apply(lambda name: Path(name).name)
+    data = pd.read_csv('driving_log2.csv', names=col)
+    data['Center'] = data['Center'].apply(lambda name: name.split('\\')[-1])
+    #print(data.shape)
     return data
 
 
 
 def balanceData(data, display=False):
-    nBins = 31
-    samplesPerBin = 500
-    historgam, bins = np.histogram(data['Steering'], nBins)
+    size = 1000
+    nbins = 31
+    
+    _, bins = pd.cut(data['Steering'], nbins, retbins=True)
+    
     if display:
-        
-        center = (bins[:-1] + bins[1:]) * .5
-        plt.bar(center, historgam, width = .06)
-        plt.plot((-1,1), (samplesPerBin, samplesPerBin))
+        plt.hist(data['Steering'], nbins)
+        plt.plot((-1,1), (size, size))
         plt.show()
-    #remove data
-    removeIndexList = []
-    for j in range(nBins):
-        binDataList = []
-        for i in range(len(data['Steering'])):
-            if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j+1]:
-                binDataList.append(i)
-        binDataList = shuffle(binDataList)
-        binDataList = binDataList[samplesPerBin:]
-        removeIndexList.extend(binDataList)
-    data.drop(data.index[removeIndexList], inplace = True)
-    print('Remaining', len(data))
+    
+    removeList = []
+    for i in range(nbins):
+        binList = []
+        for ind, d in data.iterrows():
+            if d.Steering >= bins[i] and d.Steering < bins[i+1]:
+                binList.append(ind)
+        random.shuffle(binList)
+        removeList.extend(binList[size:])
+    data.drop(data.index[removeList], inplace=True)
+    
+    if display:
+        plt.hist(data['Steering'], nbins)
+        plt.plot((-1,1), (size, size))
+        plt.show()
     return data
 
 
@@ -52,37 +54,68 @@ def loadData(path,data):
     steering = []
     for i in range(len(data)):
         indexedData = data.iloc[i]
-        imgPath.append(os.path.join(path, 'IMG', indexedData[0]))
+        imgPath.append(Path(path, 'IMG', indexedData[0]))
         steering.append(float(indexedData[3]))
-    imgPath = np.asarray(imgPath)
-    steering = np.asarray(steering)
+    imgPath, steering = np.asarray(imgPath), np.asarray(steering)
     return imgPath, steering
 
 
 def augmentImage(imgPath, steering):
     img = mpimg.imread(imgPath)
     if np.random.rand() < .5:
-        pan = iaa.Affine(translate_percent={'x':(-.1, .1), 'y':(-.1, .1)})
-        img = pan.augment_image(img)
+        img = translate(img)
     if np.random.rand() < .5:
-        zoom = iaa.Affine(scale=(1,1.2))
-        img = zoom.augment_image(img)
-    if np.random.rand() < .5:
-        brightness = iaa.Multiply((.4, 1.2))
-        img = brightness.augment_image(img)
+        img = zoom(img)
     
     if np.random.rand() < .5:
-        flip = cv2.flip(img, 1)
-        steering = -steering
-
+        img = brightness(img)
+    
+    if np.random.rand() < .5:
+        img, steering = flip(img, steering)
+    
+    if np.random.rand() < .5:
+        img = tilt(img)
     return img, steering
+
+
+
+def translate(img):
+    x = random.random() * .2 - .05
+    y = random.random() * .2 - .05
+    M= np.float32([[1, 0, x], [0, 1, y]])
+    height, width = img.shape[:2]
+    image = cv2.warpAffine(img, M, (width, height))
+    return image
+
+def zoom(img):
+    x = 1 + random.random() * .2
+    image = cv2.resize(img,None,fx=x, fy=x, interpolation = cv2.INTER_LINEAR)
+    return image
+
+def brightness(img):
+    brightness = iaa.Multiply((.4, 1.2))
+    image = brightness.augment_image(img)
+    return image
+
+def flip(img, steering):
+    image = cv2.flip(img, 1)
+    return image, -steering
+
+def tilt(img):
+    angle = random.random()*2 - 1
+    (h, w) = img.shape[:2]
+    center = (w/2, h/2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    image = cv2.warpAffine(img, M, (w, h))
+    return image
+
 
 def preProcessing(img):
     img = img[60: 135, :, :]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
     img = cv2.GaussianBlur(img, (3,3), 0)
     img = cv2.resize(img, (200,66))
-    img = img / 255.0
+    img = img / 255
     return img
 
 def batch_gen(path, steeringList, batch, train):
@@ -97,10 +130,12 @@ def batch_gen(path, steeringList, batch, train):
             else:
                 img = mpimg.imread(path[index])
                 steering = steeringList[index]
+            
             img = preProcessing(img)
             imgBatch.append(img)
             steeringBatch.append(steering)
-        yield (np.asarray(imgBatch), np.asarray(steeringBatch))
+        imgBatch, steeringBatch = np.asarray(imgBatch), np.asarray(steeringBatch)
+        yield imgBatch, steeringBatch
 
 
 
